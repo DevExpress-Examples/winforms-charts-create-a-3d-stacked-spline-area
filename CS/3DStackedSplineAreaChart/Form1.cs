76f2b62cce38a58a708ba3991d59506edb6fe3d8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/3DStackedSplineAreaChart/Form1.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+// ...
+
+namespace _DStackedSplineAreaChart {
+    public partial class Form1: Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Create an empty chart.
+            ChartControl stackedSplineArea3DChart = new ChartControl();
+
+            // Create two series of the StackedSplineArea3D view type.
+            Series series1 = new Series("Series 1", ViewType.StackedSplineArea3D);
+            Series series2 = new Series("Series 2", ViewType.StackedSplineArea3D);
+
+            // Populate both series with points.
+            series1.Points.Add(new SeriesPoint("A", 80));
+            series1.Points.Add(new SeriesPoint("B", 20));
+            series1.Points.Add(new SeriesPoint("C", 50));
+            series1.Points.Add(new SeriesPoint("D", 30));
+            series2.Points.Add(new SeriesPoint("A", 40));
+            series2.Points.Add(new SeriesPoint("B", 60));
+            series2.Points.Add(new SeriesPoint("C", 20));
+            series2.Points.Add(new SeriesPoint("D", 80));
+
+            // Add the series to the chart.
+            stackedSplineArea3DChart.Series.AddRange(new Series[] {
+                series1,
+                series2});
+
+            // Adjust the view-type-specific options of the series.
+            ((StackedSplineArea3DSeriesView)series1.View).LineTensionPercent = 10;
+            ((StackedSplineArea3DSeriesView)series2.View).LineTensionPercent = 90;
+
+            // Access the diagram's options.
+            ((XYDiagram3D)stackedSplineArea3DChart.Diagram).ZoomPercent = 110;
+
+            // Add a title to the chart and hide the legend.
+            ChartTitle chartTitle1 = new ChartTitle();
+            chartTitle1.Text = "3D Stacked Spline Area Chart";
+            stackedSplineArea3DChart.Titles.Add(chartTitle1);
+            stackedSplineArea3DChart.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False;
+
+            // Add the chart to the form.
+            stackedSplineArea3DChart.Dock = DockStyle.Fill;
+            this.Controls.Add(stackedSplineArea3DChart);
+        }
+    }
+}
+
