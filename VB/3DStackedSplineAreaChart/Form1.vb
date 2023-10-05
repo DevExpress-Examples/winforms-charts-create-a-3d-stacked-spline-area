9bf7f8d2bdd8143c5f6b24bf53aa5952d37c3cbb
DevExpressExampleBot <fakeEmail>
2023-10-05 14:01:20 +0400

    Source auto update [skip ci]

--- VB/3DStackedSplineAreaChart/Form1.vb
+++ VB/3DStackedSplineAreaChart/Form1.vb
@@ -1,24 +1,23 @@
 Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraCharts
-' ...
 
+' ...
 Namespace _DStackedSplineAreaChart
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
+
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) _
-        Handles MyBase.Load
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             ' Create an empty chart.
-            Dim stackedSplineArea3DChart As New ChartControl()
-
+            Dim stackedSplineArea3DChart As ChartControl = New ChartControl()
             ' Create two series of the StackedSplineArea3D view type.
-            Dim series1 As New Series("Series 1", ViewType.StackedSplineArea3D)
-            Dim series2 As New Series("Series 2", ViewType.StackedSplineArea3D)
-
+            Dim series1 As Series = New Series("Series 1", ViewType.StackedSplineArea3D)
+            Dim series2 As Series = New Series("Series 2", ViewType.StackedSplineArea3D)
             ' Populate both series with points.
             series1.Points.Add(New SeriesPoint("A", 80))
             series1.Points.Add(New SeriesPoint("B", 20))
@@ -28,27 +27,21 @@ Namespace _DStackedSplineAreaChart
             series2.Points.Add(New SeriesPoint("B", 60))
             series2.Points.Add(New SeriesPoint("C", 20))
             series2.Points.Add(New SeriesPoint("D", 80))
-
             ' Add the series to the chart.
             stackedSplineArea3DChart.Series.AddRange(New Series() {series1, series2})
-
             ' Adjust the view-type-specific options of the series.
             CType(series1.View, StackedSplineArea3DSeriesView).LineTensionPercent = 10
             CType(series2.View, StackedSplineArea3DSeriesView).LineTensionPercent = 90
-
             ' Access the diagram's options.
             CType(stackedSplineArea3DChart.Diagram, XYDiagram3D).ZoomPercent = 110
-
             ' Add a title to the chart and hide the legend.
-            Dim chartTitle1 As New ChartTitle()
+            Dim chartTitle1 As ChartTitle = New ChartTitle()
             chartTitle1.Text = "3D Stacked Spline Area Chart"
             stackedSplineArea3DChart.Titles.Add(chartTitle1)
             stackedSplineArea3DChart.Legend.Visible = False
-
             ' Add the chart to the form.
             stackedSplineArea3DChart.Dock = DockStyle.Fill
             Me.Controls.Add(stackedSplineArea3DChart)
         End Sub
     End Class
 End Namespace
-
